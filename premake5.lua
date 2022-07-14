83bed66c279fda96bc22b6aefab7d8f9f6531a0c
KennyProgrammer <kennytuts@gmail.com>
2022-07-14 07:12:46 +0300

    Update Premake5.lua file.

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,17 @@
+--
+-- Main Premake5 file for building OpenGL wtih GLAD project.
+-- Copyright (c) 2020-2022 by Danil (Kenny) Dukhovenko, All rights reserved.
+--
+
+-- OpenGL/GLAD C++ Project
 project "OpenGl"
 	kind "StaticLib"
 	language "C"
 	staticruntime "on"
-
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
+	files {
 		"include/glad/glad.h",
 		"include/glad/glad_wgl.h",
 		"include/KHR/khrplatform.h",
@@ -15,8 +19,7 @@ project "OpenGl"
 		"src/glad_wgl.c"
 	}
 
-	includedirs
-	{
+	includedirs {
 		"include"
 	}
 
